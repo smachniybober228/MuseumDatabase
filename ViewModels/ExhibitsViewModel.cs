0e1eee7f0dd2730e211f1328ab842370f9cf9f37
DobrovolskiyIV <DobrovolskiyIV@209D-28>
2026-06-04 15:55:25 +0700

    Editing is working

--- ViewModels/ExhibitsViewModel.cs
+++ ViewModels/ExhibitsViewModel.cs
@@ -2,6 +2,7 @@
 using CommunityToolkit.Mvvm.Input;
 using Museum.Models;
 using Museum.Repository;
+using Museum.Views;
 using System.Collections.ObjectModel;
 using System.Windows;
 
@@ -10,6 +11,8 @@ namespace Museum.ViewModels
     public partial class ExhibitsViewModel : ObservableObject
     {
         private readonly IRepository<Exhibit> _exhibitRepository;
+        private readonly IRepository<ExhibitStatus> _statusRepository;
+        private readonly IRepository<ReceiptAct> _receiptActRepository;
 
         [ObservableProperty]
         private ObservableCollection<Exhibit> _exhibits;
@@ -20,9 +23,13 @@ namespace Museum.ViewModels
         [ObservableProperty]
         private bool _isLoading;
 
-        public ExhibitsViewModel(IRepository<Exhibit> exhibitRepository)
+        public ExhibitsViewModel(IRepository<Exhibit> exhibitRepository,
+                                 IRepository<ExhibitStatus> statusRepository,
+                                 IRepository<ReceiptAct> receiptActRepository)
         {
             _exhibitRepository = exhibitRepository;
+            _statusRepository = statusRepository;
+            _receiptActRepository = receiptActRepository;
             LoadExhibitsAsync();
         }
 
@@ -49,7 +56,41 @@ namespace Museum.ViewModels
                 MessageBox.Show("Выберите экспонат для редактирования.", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Warning);
                 return;
             }
-            // ...
+            // Создаём копию для редактирования, чтобы не портить оригинал при отмене
+            var copy = new Exhibit
+            {
+                Id = SelectedExhibit.Id,
+                InventoryNumber = SelectedExhibit.InventoryNumber,
+                Title = SelectedExhibit.Title,
+                LengthCm = SelectedExhibit.LengthCm,
+                WidthCm = SelectedExhibit.WidthCm,
+                HeightCm = SelectedExhibit.HeightCm,
+                CreationDate = SelectedExhibit.CreationDate,
+                Author = SelectedExhibit.Author,
+                OriginPlace = SelectedExhibit.OriginPlace,
+                ExhibitStatusFk = SelectedExhibit.ExhibitStatusFk,
+                ReceiptActFk = SelectedExhibit.ReceiptActFk
+            };
+
+            var dialog = new ExhibitEditWindow(copy, _statusRepository, _receiptActRepository);
+            if (dialog.ShowDialog() == true)
+            {
+                // Копируем изменённые данные обратно в оригинал
+                SelectedExhibit.InventoryNumber = copy.InventoryNumber;
+                SelectedExhibit.Title = copy.Title;
+                SelectedExhibit.LengthCm = copy.LengthCm;
+                SelectedExhibit.WidthCm = copy.WidthCm;
+                SelectedExhibit.HeightCm = copy.HeightCm;
+                SelectedExhibit.CreationDate = copy.CreationDate;
+                SelectedExhibit.Author = copy.Author;
+                SelectedExhibit.OriginPlace = copy.OriginPlace;
+                SelectedExhibit.ExhibitStatusFk = copy.ExhibitStatusFk;
+                SelectedExhibit.ReceiptActFk = copy.ReceiptActFk;
+
+                _exhibitRepository.Update(SelectedExhibit);
+                await _exhibitRepository.SaveAsync();
+                await LoadExhibitsAsync();
+            }
         }
 
         [RelayCommand]
---
+++ Views/ExhibitEditWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using Museum.Models;
+using Museum.Repository;
+
+namespace Museum.Views
+{
+    public partial class ExhibitEditWindow : Window
+    {
+        public Exhibit EditedExhibit { get; private set; }
+
+        private readonly IRepository<ExhibitStatus> _statusRepo;
+        private readonly IRepository<ReceiptAct> _receiptActRepo;
+        private ObservableCollection<ExhibitStatus> _statuses;
+        private ObservableCollection<ReceiptAct> _receiptActs;
+
+        public ExhibitEditWindow(Exhibit exhibit,
+                                 IRepository<ExhibitStatus> statusRepo,
+                                 IRepository<ReceiptAct> receiptActRepo)
+        {
+            InitializeComponent();
+            EditedExhibit = exhibit;
+            _statusRepo = statusRepo;
+            _receiptActRepo = receiptActRepo;
+            Loaded += async (s, e) =>
+            {
+                // Небольшая задержка, чтобы UI успел отрисоваться
+                await Task.Delay(10);
+                await LoadDataAsync();
+            };
+            DataContext = this;
+        }
+
+        private async Task LoadDataAsync()
+        {
+            _statuses = new ObservableCollection<ExhibitStatus>(await _statusRepo.GetAllAsync());
+            _receiptActs = new ObservableCollection<ReceiptAct>(await _receiptActRepo.GetAllAsync());
+
+            if (cmbStatus != null)
+            {
+                cmbStatus.ItemsSource = _statuses;
+                cmbStatus.SelectedItem = _statuses.FirstOrDefault(s => s.Id == EditedExhibit.ExhibitStatusFk);
+            }
+
+            if (cmbReceiptAct != null)
+            {
+                cmbReceiptAct.ItemsSource = _receiptActs;
+                cmbReceiptAct.SelectedItem = _receiptActs.FirstOrDefault(r => r.Id == EditedExhibit.ReceiptActFk);
+            }
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            if (cmbStatus.SelectedItem is ExhibitStatus selectedStatus)
+                EditedExhibit.ExhibitStatusFk = selectedStatus.Id;
+            if (cmbReceiptAct.SelectedItem is ReceiptAct selectedAct)
+                EditedExhibit.ReceiptActFk = selectedAct.Id;
+
+            DialogResult = true;
+            Close();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+
+        // Поля для привязки в XAML
+        public Exhibit ExhibitData => EditedExhibit;
+        public ObservableCollection<ExhibitStatus> Statuses => _statuses;
+        public ObservableCollection<ReceiptAct> ReceiptActs => _receiptActs;
+    }
+}
